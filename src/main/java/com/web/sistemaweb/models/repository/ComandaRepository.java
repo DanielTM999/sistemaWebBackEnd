52ca5e353fcf2a912c46d46cde9b7889bd2ff25f
= <=>
2023-11-22 07:48:38 -0300

    Changes to be committed: 	modified:   src/main/java/com/web/sistemaweb/models/repository/ComandaRepository.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/web/sistemaweb/models/repository/ComandaRepository.java
+++ src/main/java/com/web/sistemaweb/models/repository/ComandaRepository.java
@@ -1,9 +1,11 @@
 package com.web.sistemaweb.models.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.web.sistemaweb.models.Comanda;
 
+@Repository
 public interface ComandaRepository extends JpaRepository<Comanda, Long>{
     
 }
