bc9435d71a6a8dae9016e3bf6ad38b2a4bd07ce7
= <=>
2023-11-21 22:01:22 -0300

    modified:   src/main/java/com/web/sistemaweb/services/ComandaServices.java

--- src/main/java/com/web/sistemaweb/services/ComandaServices.java
+++ src/main/java/com/web/sistemaweb/services/ComandaServices.java
@@ -3,13 +3,10 @@ package com.web.sistemaweb.services;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
 import com.web.sistemaweb.components.dto.req.AlterAddDto;
 import com.web.sistemaweb.components.dto.req.AlterComandaDto;
 import com.web.sistemaweb.components.dto.req.PedidosReq;
